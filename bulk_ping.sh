35e514171688d429ad91b009d07ae8cf5256fed6
Naveen J <nvnj1998@gmail.com>
2021-02-24 16:45:55 +0530

    Update bulk_ping.sh

--- bulk_ping.sh
+++ bulk_ping.sh
@@ -30,13 +30,25 @@ printf "\033[1;93m"
 now="$(date)"
 echo -e "\e[45m\033[1;97m Date: "$now"\033[0;37m"
 echo -e "   "
-echo -e "\e[41m\033[1;97m Initiating Ping probe... \033[0;37m"
-echo -e "   "
+
+# Initiation bar
+init_bar() {
+    BAR='Initiating Ping probe...'  
+    for i in {1..24}; do
+        echo -ne "\e[41m\033[1;97m \r${BAR:0:$i} \033[0;37m" 
+        sleep 0.01                 
+    done
+    echo " "
+}
+init_bar
 sleep 0.5
+echo " "
 
 #check file and get data to ping
-cat $1 |  while read output
-do
+Ping ()
+{
+  cat $tar |  while read output
+  do
     echo -e "\e[40m\033[1;97m Sending Ping request to \e[105m\033[1;97m $output \033[0;37m"
     echo "---------------------------------------------------"
     ping -c 1 "$output" > /dev/null
@@ -56,3 +68,15 @@ rm tmp1.txt tmp2.txt
 echo -e "\e[102m\033[1;97m Removed Temporary files...\033[0;37m"
 echo " "
 echo -e "\e[45m\033[1;97m All done \033[0;37m"
+}
+
+# Validate input
+if [ $# -eq 0 ]
+then
+    echo " "
+    echo -e "\e[5m\e[41m\033[1;97m No target files supplied \033[0;37m"
+    exit -1
+else
+    tar=$1
+    Ping
+fi
