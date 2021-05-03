634e7f023d6768e227dc7cf72307e289c36716dc
Naveen J <nvnj1998@gmail.com>
2021-05-03 07:54:52 +0530

    Update bulk_ping.sh

--- bulk_ping.sh
+++ bulk_ping.sh
@@ -47,7 +47,7 @@ echo " "
 #check file and get data to ping
 Ping ()
 {
-  mkdir bpingresults
+  mkdir results
   cat $tar |  while read output
   do
     echo -e "\e[40m\033[1;97m Sending Ping request to \e[105m\033[1;97m $output \033[0;37m"
@@ -55,17 +55,17 @@ Ping ()
     ping -c 1 "$output" > /dev/null
     if [ $? -eq 0 ]; then
     echo -e "\e[42m\033[1;97m [+] node [$output] is up \033[0;37m" 
-    echo "$output" >>bpingresults/tmp1.txt
-    cat bpingresults/tmp1.txt | sort -u >bpingresults/up.txt
+    echo "$output" >>results/tmp1.txt
+    cat results/tmp1.txt | sort -u >results/up.txt
     echo " "
     else
     echo -e "\e[5m\e[41m\033[1;97m [-] node [$output] is down \033[0;37m"
-    echo "$output" >>bpingresults/tmp2.txt
-    cat bpingresults/tmp2.txt | sort -u >bpingresults/down.txt
+    echo "$output" >>results/tmp2.txt
+    cat results/tmp2.txt | sort -u >results/down.txt
     echo " "
     fi
 done
-cd bpingresults
+cd results
 rm tmp1.txt tmp2.txt
 echo -e "\e[41m\033[1;97m Removed Temporary files...\033[0;37m"
 echo " "
